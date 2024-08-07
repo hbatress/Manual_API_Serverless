ee92bc567ad155ced0cea3208f1ba078fe0361fd
herber <hbatress@miumg.edu.gt>
2024-08-07 13:35:59 -0600

    AWS SDK para Java

---
+++ bacets.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+
+// Configurar la región
+AWS.config.update({region: 'us-east-1'});
+
+// Crear una instancia de S3
+const s3 = new AWS.S3();
+
+// Llamar al método listBuckets
+s3.listBuckets((err, data) => {
+    if (err) console.log(err, err.stack); // un error ocurrió
+    else console.log(data.Buckets); // éxito
+});
