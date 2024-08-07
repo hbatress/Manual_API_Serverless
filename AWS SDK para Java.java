7d44b5ea3ff79392492bd2f063871a73a97bca02
herber <hbatress@miumg.edu.gt>
2024-08-07 17:39:51 -0600

    SDK DE JAVA

--- AWS SDK para Java.java
+++ AWS SDK para Java.java
@@ -1,10 +1,44 @@
-<dependency>
-  <groupId>software.amazon.awssdk</groupId>
-  <artifactId>bom</artifactId>
-  <version>2.17.89</version>
-  <type>pom</type>
-  <scope>import</scope>
-</dependency>
-<dependency>
-  <groupId>software.amazon.awssdk</groupId>
-  <artifactId>ec2</artifactId
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.Instance;
+import software.amazon.awssdk.services.ec2.model.Reservation;
+import software.amazon.awssdk.services.ec2.model.Ec2Exception;
+
+public class ListInstances {
+
+    public static void main(String[] args) {
+        // Crear un cliente de EC2
+        Ec2Client ec2 = Ec2Client.builder()
+                .region(Region.US_EAST_1) // Especifica la región
+                .build();
+
+        try {
+            // Crear una solicitud para describir las instancias
+            DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
+
+            // Obtener la respuesta con las instancias
+            DescribeInstancesResponse response = ec2.describeInstances(request);
+
+            // Iterar sobre las reservas y las instancias
+            for (Reservation reservation : response.reservations()) {
+                for (Instance instance : reservation.instances()) {
+                    // Imprimir la información de la instancia
+                    System.out.printf(
+                            "Instance ID: %s, Instance Type: %s, State: %s%n",
+                            instance.instanceId(),
+                            instance.instanceType(),
+                            instance.state().name()
+                    );
+                }
+            }
+
+        } catch (Ec2Exception e) {
+            System.err.println("Error al describir las instancias: " + e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        } finally {
+            ec2.close();
+        }
+    }
+}
